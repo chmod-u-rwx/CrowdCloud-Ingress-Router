74c96f6c32b073714789efada154870ee94ff429
0CottonBuds <emiljohnllanes772@gmail.com>
2025-09-09 02:52:43 +0800

    Added heartbeat endpoint and added singleton for router

---
+++ src/ingress_router/routes/heartbeat.py
@@ -0,0 +1,26 @@
+import copy
+import threading
+import time
+from fastapi import APIRouter
+from ..type_dict import Worker
+from ..router import router
+
+api_router = APIRouter(prefix="/heartbeat")
+
+heartbeat_queue: list[Worker] = []
+
+
+@api_router.post("/")
+def heartbeat(worker: Worker):
+    heartbeat_queue.append(worker)
+
+
+def consume_loop():
+    while True:
+        worker_data = copy.deepcopy(heartbeat_queue)
+        router.update_worker_data(worker_data)
+        heartbeat_queue.clear()
+        time.sleep(1)
+
+thread = threading.Thread(target=consume_loop, daemon=True)
+thread.start()
